22b0499960701de72deb56576509f738813e3f3a
Andrew Allen <aallen170@gmail.com>
2019-01-26 15:27:03 -0600

    Added Scene Management functionality

--- Assets/Scripts/MySceneManager.cs
+++ Assets/Scripts/MySceneManager.cs
@@ -20,17 +20,75 @@ public class MySceneManager : MonoBehaviour
         MainMenuSceneIndex, GameOverSceneIndex,
         DebugSceneIndex;
 
+    int currentSceneIndex;
+
 	// Use this for initialization
 	void Start ()
     {
-        currentScene = SceneList.DefaultScene;
-        DefaultSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/default.unity");
-        DebugSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/debug.unity");
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        GetSceneIndices();
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SetCurrentScene();
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
-        
+        SwitchScenes();
+    }
+
+    private void GetSceneIndices()
+    {
+        DefaultSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/Default.unity");
+        LoadingSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/Loading.unity");
+        MainMenuSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/MainMenu.unity");
+        GameOverSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/GameOver.unity");
+        DebugSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/Debug.unity");
+    }
+
+    private void SetCurrentScene()
+    {
+        if (currentSceneIndex == DefaultSceneIndex) currentScene = SceneList.DefaultScene;
+        if (currentSceneIndex == LoadingSceneIndex) currentScene = SceneList.LoadingScene;
+        if (currentSceneIndex == MainMenuSceneIndex) currentScene = SceneList.MainMenuScene;
+        if (currentSceneIndex == GameOverSceneIndex) currentScene = SceneList.GameOverScene;
+        if (currentSceneIndex == DebugSceneIndex) currentScene = SceneList.DebugScene;
+    }
+
+    private void SwitchScenes()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentScene = SceneList.MainMenuScene;
+            LoadScene();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentScene = SceneList.LoadingScene;
+            LoadScene();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentScene = SceneList.DefaultScene;
+            LoadScene();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentScene = SceneList.GameOverScene;
+            LoadScene();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            currentScene = SceneList.DebugScene;
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+        if (currentScene == SceneList.MainMenuScene) SceneManager.LoadScene(MainMenuSceneIndex);
+        if (currentScene == SceneList.LoadingScene) SceneManager.LoadScene(LoadingSceneIndex);
+        if (currentScene == SceneList.DefaultScene) SceneManager.LoadScene(DefaultSceneIndex);
+        if (currentScene == SceneList.GameOverScene) SceneManager.LoadScene(GameOverSceneIndex);
+        if (currentScene == SceneList.DebugScene) SceneManager.LoadScene(DebugSceneIndex);
     }
 }
