50e9750e14c5b407ec657fb54fe88426044ec22f
Andrew Allen <aallen170@gmail.com>
2019-01-26 14:52:00 -0600

    Update

---
+++ Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{
+    public enum SceneList
+    {
+        DefaultScene,
+        LoadingScene,
+        MainMenuScene,
+        GameOverScene,
+        DebugScene
+    }
+
+    SceneList currentScene;
+
+    int DefaultSceneIndex, LoadingSceneIndex,
+        MainMenuSceneIndex, GameOverSceneIndex,
+        DebugSceneIndex;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentScene = SceneList.DefaultScene;
+        DefaultSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/default.unity");
+        DebugSceneIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/debug.unity");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
