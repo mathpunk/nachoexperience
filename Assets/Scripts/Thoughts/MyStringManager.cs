422448d48a36e2855dc71e41eb02cab6ba70a87e
Andrew Allen <aallen170@gmail.com>
2019-01-26 17:03:49 -0600

    added a bunch of art assets and fixing conflicts

---
+++ Assets/Scripts/Thoughts/MyStringManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyStringManager : MonoBehaviour {
+
+    string sampleStr = "This is a test to see if this script actually works.";
+
+    int strIndex = 0;
+
+    float currentTime = Mathf.Epsilon, maxTime = 1f;
+
+    [Range(1, 60)]
+    [SerializeField]
+    float speechSpeed = 20f;
+
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = gameObject.GetComponent<Text>();
+        text.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        text.text += sampleStr[strIndex];
+	}
+}
