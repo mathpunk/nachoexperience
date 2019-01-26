2e766c0922e49d571c73307b332e42961d05e7b4
Aaron Stackpole <aaron@carryingthewhale.com>
2019-01-26 11:39:32 -0600

    default scene, scene manager

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    #region PlayerBehavior Private Members
+
+    GameObject m_player;
+
+    // Where am I (home room, city block)
+    // PlayerStateManger member class
+
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_player = GameObject.Find("PlayerSprite");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
