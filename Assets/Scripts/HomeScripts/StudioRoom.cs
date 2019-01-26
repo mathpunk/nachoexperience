c20501a87709e76378d182d30f0f2227b7364a8d
Andrew Allen <aallen170@gmail.com>
2019-01-26 17:01:18 -0600

    commiting for the sake of merging

--- Assets/Scripts/HomeScripts/StudioRoom.cs
+++ Assets/Scripts/HomeScripts/StudioRoom.cs
@@ -1,27 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class StudioRoom : HomeRoomBase
-{
-    private string[] m_studioHints;
+//public class StudioRoom : HomeRoomBase
+//{
+//    private string[] m_studioHints;
 
-    public string GetHint()
-    {
-        var rnd = Random.Range(0, m_studioHints.Length - 1);
-        return m_studioHints[rnd];
-    }
+//    public string GetHint()
+//    {
+//        var rnd = Random.Range(0, m_studioHints.Length - 1);
+//        return m_studioHints[rnd];
+//    }
 
-	// Use this for initialization
-	void Start ()
-    {
-        // Get strings for this room
-        m_studioHints = m_hints.StudioHints;
-	}
+//	// Use this for initialization
+//	void Start ()
+//    {
+//        // Get strings for this room
+//        m_studioHints = m_hints.StudioHints;
+//	}
 	
-	// Update is called once per frame
-	void Update ()
-    {
+//	// Update is called once per frame
+//	void Update ()
+//    {
 		
-	}
-}
+//	}
+//}
--- Assets/Scripts/Thoughts/Reactions.cs
+++ Assets/Scripts/Thoughts/Reactions.cs
@@ -1,42 +1,42 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
 
-public class Reactions
-{
-    public enum Verb { Light, Photograph, Record, Pay };
-    public enum Item { Beach, Cat, Grocer, BluesBand };
-    public enum Action { Light, Cat };
+//public class Reactions
+//{
+//    public enum Verb { Light, Photograph, Record, Pay };
+//    public enum Item { Beach, Cat, Grocer, BluesBand };
+//    public enum Action { Light, Cat };
 
-    public string GetQuip(Action action, Verb verb, Item item)
-    {
-        switch (action)
-        {
-            case Action.Light:
-            case Action.Cat:
-                return "I'll call you Mr. Whiskers. I feel less lonely already.";
+//    public string GetQuip(Action action, Verb verb, Item item)
+//    {
+//        switch (action)
+//        {
+//            case Action.Light:
+//            case Action.Cat:
+//                return "I'll call you Mr. Whiskers. I feel less lonely already.";
 
-            case Action.Beach:
-            case Action.Photograph:
-                return "The water reaches right up to the city… just like in Mar del Plata.";
+//            case Action.Beach:
+//            case Action.Photograph:
+//                return "The water reaches right up to the city… just like in Mar del Plata.";
 
-            case Action.Grocer:
-            case Action.Pay:
-                return "Chicken, garlic, parsley, tomato, lemon... and cheese, of course. I can almost taste my mother’s milanesa already."
+//            case Action.Grocer:
+//            case Action.Pay:
+//                return "Chicken, garlic, parsley, tomato, lemon... and cheese, of course. I can almost taste my mother’s milanesa already."
 
-            case Action.BluesBand:
-            case Action.Record:
-                return "Mm, yes! The blues reminds me that you can be happy even when times are tough.";
+//            case Action.BluesBand:
+//            case Action.Record:
+//                return "Mm, yes! The blues reminds me that you can be happy even when times are tough.";
 
-            default:
-                // return "Sigh... I don't know why I thought that " + Gerund(verb) + " a " + item + " would help me feel better...";
-                return "Sigh... I don't know why I thought that would help me feel less homesick."
-        }
-    }
+//            default:
+//                // return "Sigh... I don't know why I thought that " + Gerund(verb) + " a " + item + " would help me feel better...";
+//                return "Sigh... I don't know why I thought that would help me feel less homesick."
+//        }
+//    }
 
-    public string Gerund(Verb verb)
-    {
-        return verb.ToString() + "ing";
-    }
+//    public string Gerund(Verb verb)
+//    {
+//        return verb.ToString() + "ing";
+//    }
 
-}
+//}
