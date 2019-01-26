01e7fbda092678c78e6b8e1eda8c6adf00ca0f13
Andrew Allen <aallen170@gmail.com>
2019-01-26 13:56:47 -0600

    Added collision, movement, and camera controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
+
+
 using System.Collections.Generic;
 using UnityEngine;
 
-//[RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Controller2D))]
-//[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
     #region Variables
     #region Input Vars
     float horizontalInput, verticalInput;
+
+    bool jumping = false;
+
+    [Range(1, 100)]
+    [SerializeField]
+    float jumpPower = 50;
     #endregion Input Vars
 
     #region Physics Vars
-    float gravity, fallSpeed;
+    float gravity;
+
+    bool grounded;
+
+    float accelerationTimeGrounded, accelerationTimeAirborne;
+
+    [Range(1, 100)]
+    [SerializeField]
+    float groundedTraction = 50, airialTraction = 20;
+
+    [Range(1, 300)]
+    [SerializeField]
+    float fallSpeed = 100;
     #endregion Physics Vars
 
     #region Speed Vars
@@ -34,6 +52,10 @@ public class Player : MonoBehaviour
     private void Update()
     {
         BasicMovement();
+        DetectJumping();
+        ColPhysChecks();
+
+        if(velocity.y > 0) print(velocity);
     }
 
     private void BasicMovement()
@@ -41,8 +63,38 @@ public class Player : MonoBehaviour
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         velocity.x = input.x * speed;
-        velocity.y = input.y * speed;
+    }
 
+    void DetectJumping()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (controller.collisions.below && !jumping)
+            {
+                jumping = true;
+                velocity.y = jumpPower;
+            }
+        }
+    }
+
+    void ColPhysChecks()
+    {
+        grounded = controller.collisions.below;
+
+        gravity = -fallSpeed * 2f;
+        accelerationTimeGrounded = 1f / groundedTraction;
+        accelerationTimeAirborne = 1f / airialTraction;
+
+        velocity.y += gravity * Time.deltaTime;
+
+        if (controller.collisions.isAirborne()) jumping = false;
+
+        if ((controller.collisions.below && !jumping) ||
+            controller.collisions.above)
+        {
+            velocity.y = 0;
+        }
+        
         controller.Move(velocity * Time.deltaTime);
     }
 }
