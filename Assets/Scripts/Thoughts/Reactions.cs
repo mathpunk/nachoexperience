585b895fec7df504e5edbe881b94fbc20d5bdbd2
tHomas H <tom@mathpunk.net>
2019-01-26 19:25:42 -0600

    Nested case statements

--- Assets/Scripts/Thoughts/Reactions.cs
+++ Assets/Scripts/Thoughts/Reactions.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-// recovery version
-
 public class Reactions
 {
-    public enum Verb { Light, Photograph, Record, Pay };
+    public enum Verb {
+        Light,
+        Photograph,
+        Record,
+        Pay
+    };
     public enum Item {
         // Wanted items
         Beach,
@@ -24,33 +27,56 @@ public class Reactions
         FerrisWheel
     };
 
-    public string GetQuip(Action action, Verb verb, Item item)
+    public string GeneralFailure()
     {
-        switch (action)
+        return "Sigh... I don't know why I thought that would help me feel less homesick.";
+    }
+
+    public string GetQuip(Verb verb, Item item)
+    {
+        switch (item)
         {
-            case Action.Light:
-            case Action.Cat:
-                return "I'll call you Mr. Whiskers. I feel less lonely already.";
+            case Item.Cat:
+                switch (verb)
+                {
+                    case Action.Light:
+                        return "I'll call you Mr. Whiskers! I feel less lonely already.";
+                    case Action.Photograph:
+                        return "I guess I'm a guy who takes cat photos now. Huh."
+                    case Action.Record:
+                        return "C'mon kitty... Meow or purr or something... Dang."
+                    case Action.Pay:
+                        return "I don't think this cat takes cards. What a weird thing to do --- I better go home and rest."
+                    default:
+                        return GeneralFailure();
+                }
 
-            case Action.Light:
-            case Action.Rat:
-                return "Aaaaaaaaaaaaaaaaaaaaaahhhhh!!!!!";
+            case Item.Beach:
+                switch (verb)
+                {
+                    case Action.Photograph:
+                        return "A great view. The water reaches right up to the city... just like in Mar del Plata.";
+                    default:
+                        return GeneralFailure();
+                }
 
-            case Action.Light:
-            case Action.Squirrel:
-                return "Cute. But not a very good pet.";
+            case Item.Grocer:
+                switch (verb)
+                {
+                    case Action.Pay:
+                        return "Chicken, garlic, parsley, tomato, lemon... and cheese, of course. I can almost taste my mother’s milanesa already."
+                    default:
+                        return GeneralFailure();
+                }
 
-            case Action.Photograph:
-            case Action.Beach:
-                return "The water reaches right up to the city… just like in Mar del Plata.";
-
-            case Action.Pay:
-            case Action.Grocer:
-                return "Chicken, garlic, parsley, tomato, lemon... and cheese, of course. I can almost taste my mother’s milanesa already."
-
-            case Action.Record:
-            case Action.BluesBand:
-                return "Mm, yes! I love it. The blues reminds me that you can be happy even when times are tough.";
+            case Item.BluesBand:
+                switch (verb)
+                {
+                    case Action.Record:
+                        return "Yes! I love it! The blues reminds me that you can be happy even when times are tough.";
+                    default:
+                        return GeneralFailure();
+                }
 
             default:
                 return "Sigh... I don't know why I thought that would help me feel less homesick."
@@ -63,3 +89,11 @@ public class Reactions
     }
 
 }
+
+// case Action.Light:
+// case Action.Rat:
+//     return "Aaaaaaaaaaaaaaaaaaaaaahhhhh!!!!!";
+
+// case Action.Light:
+// case Action.Squirrel:
+//     return "Cute. But not a very good pet.";
